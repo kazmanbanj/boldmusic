af87baadf2c4fb90801d0874dd979cb901a71acb
Banjoko Kazeem <banjoko10@gmail.com>
2023-10-15 13:01:42 +0100

    loading assets in public folder so it won't load with third party libraries like postcss and sass

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
     <section class="mb-8 py-20 text-white text-center relative">
       <div
         class="absolute inset-0 w-full h-full bg-contain introduction-bg"
-        style="background-image: url(src/assets/img/header.png)"
+        style="background-image: url(assets/img/header.png)"
       ></div>
       <div class="container mx-auto">
         <div class="text-white main-header-content">
@@ -42,7 +42,7 @@
 
       <img
         class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
-        src="src/assets/img/introduction-music.png"
+        src="assets/img/introduction-music.png"
       />
     </section>
 
