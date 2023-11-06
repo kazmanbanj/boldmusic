6da26d0e75cf023e5298e061430e486ed42b59d3
Banjoko Kazeem <banjoko10@gmail.com>
2023-11-06 02:49:02 +0100

    custom error messages

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -80,7 +80,7 @@
           </form>
 
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register" :initial-values="userData">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -117,12 +117,20 @@
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
               <vee-field
-                type="password"
                 name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-              <ErrorMessage class="text-red-600" name="password"/>
+                :bails="false"
+                v-slot="{ field, errors }"
+              >
+                <input
+                  type="password"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  v-bind="field">
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
+              <!-- <ErrorMessage class="text-red-600" name="password"/> -->
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -181,10 +189,13 @@ export default {
         name: "required|min:3|max:100|alpha_spaces",
         email: "required|min:3|max:100|email",
         age: "required|min_value:18|max_value:100",
-        password: "required|min:3|max:100",
-        confirm_password: "confirmed:@password",
-        country: "required|excluded:Antarctica",
-        tos: "required",
+        password: "required|min:9|max:100|excluded:password",
+        confirm_password: "passwords_mismatch:@password",
+        country: "required|country_excluded:Antarctica",
+        tos: "tos_required",
+      },
+      userData: {
+        country: 'USA'
       }
     }
   },
@@ -193,6 +204,11 @@ export default {
     ...mapWritableState(useModalStore, {
       modalVisibility: 'isOpen'
     })
-  }
+  },
+  methods: {
+    register(values) {
+      console.log(values);
+    },
+  },
 }
 </script>
