30b9b209838c2e8c7705ed02984018ac4cd499a7
Banjoko Kazeem <banjoko10@gmail.com>
2023-11-10 06:58:54 +0100

    auth with firebase

---
+++ src/includes/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
+};
+
+export default firebase.initializeApp(firebaseConfig);
