1faa88af5d5efc227e7b53f1c5d28ac10033cd08
Banjoko Kazeem <banjoko10@gmail.com>
2023-12-03 04:42:27 +0100

    using pinia actions to make codes reusable

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -99,9 +99,8 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase';
-import { mapWritableState } from "pinia";
-import { useUserStore } from "@/stores/user";
+import { mapActions } from "pinia";
+import useUserStore from "@/stores/user";
 
 export default {
     name: 'RegisterForm',
@@ -126,21 +125,18 @@ export default {
             reg_alert_msg: "Please wait, Your account is being created.",
         }
     },
-    computed: {
-        ...mapWritableState(useUserStore, ["userLoggedIn"])
-    },
     methods: {
+        ...mapActions(useUserStore, {
+            createUser: "register"
+        }),
         async register(values) {
             this.reg_show_alert = true;
             this.reg_in_submission = true;
             this.reg_alert_variant = "bg-blue-500";
             this.reg_alert_msg = "Please wait, Your account is being created.";
 
-            let userCred = null;
             try {
-                userCred = await auth.createUserWithEmailAndPassword(
-                    values.email, values.password
-                );
+                await this.createUser(values);
             } catch (error) {
                 this.reg_in_submission = false;
                 this.reg_alert_variant = "bg-red-500";
@@ -148,28 +144,9 @@ export default {
                 console.log(error);
                 return;
             }
-
-            try {
-                await usersCollection.add({
-                    name: values.name,
-                    email: values.email,
-                    age: values.age,
-                    country: values.country
-                });
-            } catch (error) {
-                this.reg_in_submission = false;
-                this.reg_alert_variant = "bg-red-500";
-                this.reg_alert_msg = error.message;
-                console.log(error);
-                return;
-            }
-
-            this.userLoggedIn = true;
 
             this.reg_alert_variant = "bg-green-500";
             this.reg_alert_msg = "Success! Your account has been created."
-
-            console.log(userCred);
         },
     },
 }
