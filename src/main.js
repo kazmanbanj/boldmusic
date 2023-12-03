35e4139c160feff86c9f85fa6f196711efef7e76
Banjoko Kazeem <banjoko10@gmail.com>
2023-12-03 09:46:39 +0100

    initializing firebase first before vue to keep the user logged in

--- src/main.js
+++ src/main.js
@@ -4,15 +4,20 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import VeeValidate from './includes/validation';
-import "./includes/firebase"
+import { auth } from "./includes/firebase"
 
 import './assets/base.css';
 import './assets/main.css';
 
-const app = createApp(App);
+let app;
+auth.onAuthStateChanged(() => {
+    if (!app) {
+        app = createApp(App);
 
-app.use(createPinia());
-app.use(router);
-app.use(VeeValidate);
+        app.use(createPinia());
+        app.use(router);
+        app.use(VeeValidate);
 
-app.mount('#app')
+        app.mount('#app')
+    }
+})
