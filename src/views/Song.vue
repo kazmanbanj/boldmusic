7124365325400088a5050d1bddeb74eefb405cdd
Banjoko Kazeem <banjoko10@gmail.com>
2024-01-14 17:14:24 +0100

    comments list

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -52,76 +52,14 @@
     </section>
     <!-- Comments -->
     <ul class="container mx-auto">
-        <li class="p-6 bg-gray-50 border border-gray-200">
+        <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in comments" :key="comment.docID">
             <!-- Comment Author -->
             <div class="mb-5">
-            <div class="font-bold">Elaine Dreyfuss</div>
-            <time>5 mins ago</time>
+            <div class="font-bold">{{ comment.name }}</div>
+            <time>{{ comment.datePosted }}</time>
             </div>
 
-            <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium der doloremque laudantium.
-            </p>
-        </li>
-        <li class="p-6 bg-gray-50 border border-gray-200">
-            <!-- Comment Author -->
-            <div class="mb-5">
-            <div class="font-bold">Elaine Dreyfuss</div>
-            <time>5 mins ago</time>
-            </div>
-
-            <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium der doloremque laudantium.
-            </p>
-        </li>
-        <li class="p-6 bg-gray-50 border border-gray-200">
-            <!-- Comment Author -->
-            <div class="mb-5">
-            <div class="font-bold">Elaine Dreyfuss</div>
-            <time>5 mins ago</time>
-            </div>
-
-            <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium der doloremque laudantium.
-            </p>
-        </li>
-        <li class="p-6 bg-gray-50 border border-gray-200">
-            <!-- Comment Author -->
-            <div class="mb-5">
-            <div class="font-bold">Elaine Dreyfuss</div>
-            <time>5 mins ago</time>
-            </div>
-
-            <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium der doloremque laudantium.
-            </p>
-        </li>
-        <li class="p-6 bg-gray-50 border border-gray-200">
-            <!-- Comment Author -->
-            <div class="mb-5">
-            <div class="font-bold">Elaine Dreyfuss</div>
-            <time>5 mins ago</time>
-            </div>
-
-            <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium der doloremque laudantium.
-            </p>
-        </li>
-        <li class="p-6 bg-gray-50 border border-gray-200">
-            <!-- Comment Author -->
-            <div class="mb-5">
-            <div class="font-bold">Elaine Dreyfuss</div>
-            <time>5 mins ago</time>
-            </div>
-
-            <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium der doloremque laudantium.
-            </p>
+            <p>{{ comment.content }}</p>
         </li>
     </ul>
 </template>
@@ -142,7 +80,8 @@ export default {
             comment_in_submission: false,
             comment_show_alert: false,
             comment_alert_variant: 'bg-blue-500',
-            comment_alert_message: 'Please wait! Your comment is being submitted.'
+            comment_alert_message: 'Please wait! Your comment is being submitted.',
+            comments: []
         }
     },
     computed: {
@@ -155,6 +94,7 @@ export default {
             return;
         }
         this.song = docSnapshot.data();
+        this.getComments();
     },
     methods: {
         async addComment(values, { resetForm }) {
@@ -179,6 +119,17 @@ export default {
 
             resetForm();
         },
+        async getComments() {
+            const snapshots = await commentsCollection.where('sid', '==', this.$route.params.id).get();
+
+            this.comments = [];
+            snapshots.forEach((doc) => [
+                this.comments.push({
+                    docID: doc.id,
+                    ...doc.data(),
+                })
+            ]);
+        }
     },
 }
 </script>
