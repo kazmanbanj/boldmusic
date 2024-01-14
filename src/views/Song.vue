44109bd8bec271b87c3f3506d6fe1b0ca011b4c1
Banjoko Kazeem <banjoko10@gmail.com>
2024-01-14 17:35:56 +0100

    sorting the commentss

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -42,8 +42,8 @@
             </vee-form>
             <!-- Sort Comments -->
             <select
-                class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-            >
+            v-model="sort"
+            class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded">
                 <option value="1">Latest</option>
                 <option value="2">Oldest</option>
             </select>
@@ -52,7 +52,7 @@
     </section>
     <!-- Comments -->
     <ul class="container mx-auto">
-        <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in comments" :key="comment.docID">
+        <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in sortedComments" :key="comment.docID">
             <!-- Comment Author -->
             <div class="mb-5">
             <div class="font-bold">{{ comment.name }}</div>
@@ -81,11 +81,21 @@ export default {
             comment_show_alert: false,
             comment_alert_variant: 'bg-blue-500',
             comment_alert_message: 'Please wait! Your comment is being submitted.',
-            comments: []
+            comments: [],
+            sort: "1"
         }
     },
     computed: {
         ...mapState(useUserStore, ["userLoggedIn"]),
+        sortedComments() {
+            return this.comments.slice().sort((a, b) => {
+                if (this.sort === "1") {
+                    return new Date(b.datePosted) - new Date(a.datePosted);
+                }
+
+                return new Date(a.datePosted) - new Date(b.datePosted);
+            })
+        }
     },
     async created() {
         const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
@@ -112,6 +122,8 @@ export default {
             };
 
             await commentsCollection.add(comment);
+
+            this.getComments();
 
             this.comment_in_submission = false;
             this.comment_alert_variant = 'bg-green-500';
