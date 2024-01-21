4ec03dc72e717b8548dea731283e259769ff087c
Banjoko Kazeem <banjoko10@gmail.com>
2024-01-21 15:22:40 +0100

    transition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+  <main>
     <!-- Introduction -->
     <section class="mb-8 py-20 text-white text-center relative">
       <div
@@ -42,6 +43,7 @@
         <!-- .. end Playlist -->
       </div>
     </section>
+  </main>
 </template>
 
 <script>
