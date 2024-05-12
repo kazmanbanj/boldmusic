a2ca164d7c650041770823138dce228cbfe53605
Banjoko Kazeem <banjoko10@gmail.com>
2024-05-12 11:46:13 +0100

    perceived performance

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { auth } from "./includes/firebase"
 import Icon from './directives/icon';
 import i18n from './includes/i18n';
 import { registerSW } from "virtual:pwa-register";
+import GlobalComponent from "./includes/_globals";
 
 import './assets/base.css';
 import './assets/main.css';
@@ -24,6 +25,7 @@ auth.onAuthStateChanged(() => {
         app.use(router);
         app.use(VeeValidate);
         app.use(i18n);
+        app.use(GlobalComponent);
         app.directive("icon", Icon);
 
         app.mount('#app')
